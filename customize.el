409382ec1cba961d37e5e344dfc091a79ad4da53
bkc39 <bkc39@cornell.edu>
2014-08-13 19:52:51 -0500

    Add haskell customization.

--- customize.el
+++ customize.el
@@ -66,14 +66,48 @@
                 ("\\.lhs$" . haskell-mode))
               auto-mode-alist))
 
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 (add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
-(add-hook 'haskell-mode-hook '(lambda ()
-				(require 'haskell-mode)
-				(define-key haskell-mode-map "C-c h" 'haskell-hoogle)
-				(setq haskell-hoogle-command "hoogle")))
+(add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
+(add-hook 'haskell-mode-hook
+          '(lambda ()
+             ;; Flycheck
+             (flycheck-select-checker 'haskell-hlint)
+             ;; Set up hoogle
+             (define-key haskell-mode-map "C-c h" 'haskell-hoogle)
+             (setq haskell-hoogle-command "hoogle")
+             (define-key haskell-mode-map (kbd "M-[") 'align)
+             (define-key haskell-mode-map [f8] 'haskell-navigate-imports)))
 
+;; Flycheck stuff
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; For indenting entire code blocks
+(eval-after-load "haskell-mode"
+  '(progn
+     (define-key haskell-mode-map (kbd "C-,") 'haskell-move-nested-left)
+     (define-key haskell-mode-map (kbd "C-.") 'haskell-move-nested-right)))
+
+;; For Interactive Haskell Mode
+(custom-set-variables
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log)
+ '(haskell-tags-on-save t)
+ '(haskell-stylish-on-save t)
+ '(haskell-process-suggest-hoogle-imports t)
+ '(haskell-process-type 'cabal-repl))
+
+;; REPL keyboard shortcuts
+(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+ 
 ;; AUCTeX
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
