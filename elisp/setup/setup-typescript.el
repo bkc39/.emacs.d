7afb7e8cda1b88c392bf583b2a3bb17b030b7df5
bkc39 <pl.proofs@gmail.com>
2018-11-25 13:48:35 -0500

    Move tide formatting into setup-typescript hook

--- elisp/setup/setup-typescript.el
+++ elisp/setup/setup-typescript.el
@@ -5,10 +5,10 @@
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1)
+  (add-hook 'before-save-hook 'tide-format-before-save)
+  (setq company-tooltip-align-annotations 1)
   (company-mode +1))
 
-(setq company-tooltip-align-annotations 1)
-(add-hook 'before-save-hook 'tide-format-before-save)
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 (require 'web-mode)
