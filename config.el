ba4bdee76e0e201d70bc1c10074da53946036ed8
Ben Carriel <pl.proofs@gmail.com>
2022-04-01 15:41:02 -0400

    start doing haskell mode config

--- config.el
+++ config.el
@@ -20,4 +20,28 @@
     (enable-theme 'zenburn)))
 
 (use-package haskell-mode
-  :mode "\\.hs$")
+  :mode "\\.hs$"
+  :init
+  (progn
+    (setq haskell-process-args-ghci
+          '("-ferror-spans" "-fshow-loaded-modules"))
+    (setq haskell-process-args-cabal-repl
+          '("--ghc-options=-ferror-spans -fshow-loaded-modules"))
+    (setq haskell-process-args-stack-ghci
+          '("--ghci-options=-ferror-spans"
+            " -fshow-loaded-modules"
+            "--no-build"
+            "--no-load")))
+  :bind (:map haskell-mode-map
+              ("C-c C-l" . haskell-process-load-file)
+              ("C-c C-z" . haskell-interactive-switch)
+              ("C-`" . haskell-interactive-bring)
+              ("C-c C-c" . haskell-process-cabal-build)
+              ("C-c C-t" . haskell-process-do-type)
+              ("C-c C-i" . haskell-process-do-info)
+              ("C-c C-<right>" . (lambda ()
+                                   (interactive)
+                                   (haskell-move-nested 1)))
+              ("C-c C-<left>" . (lambda ()
+                                  (interactive)
+                                  (haskell-mode-nested -1)))))
