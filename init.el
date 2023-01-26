f9cb39a42f9aee4cf6f626148e9b7e27e6324fc0
bkc39 <pl.proofs@gmail.com>
2023-01-26 12:48:28 -0500

    custom set variables stuff

--- init.el
+++ init.el
@@ -145,3 +145,16 @@
                 (lambda ()
                   (interactive)
                   (other-window -1)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(zenburn-theme swift-mode racket-mode proof-general paredit multiple-cursors magit lsp-sourcekit lsp-pyright lsp-mode ess exec-path-from-shell company-coq dash use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
