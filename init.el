5f60d91c48517396a81782562c74fe5839079166
bkc39 <pl.proofs@gmail.com>
2023-08-10 10:54:58 -0400

    wip: start chatgpt integration

--- init.el
+++ init.el
@@ -34,21 +34,57 @@
   :hook (coq-mode . company-coq-mode)
   :init (setq company-coq-live-on-the-edge t))
 
-(use-package exec-path-from-shell
-  :if (memq window-system '(mac ns))
-  :ensure t
-  :config
-  (exec-path-from-shell-initialize))
+(use-package cmake-mode
+  :ensure t)
+
+(use-package ein
+  :ensure t)
 
 (use-package ess
   :ensure t
   :mode ("\\.R\\'" . R-mode))
 
+(use-package exec-path-from-shell
+  :if (or (memq window-system '(mac ns))
+          (eq system-type 'darwin))
+  :ensure t
+  :config
+  (exec-path-from-shell-initialize))
+
+(use-package flycheck
+  :ensure t)
+
+(use-package chatgpt
+  :straight (:host github
+                   :repo "joshcho/ChatGPT.el"
+                   :files ("dist" "*.el"))
+
+  :bind ("C-c q" . chatgpt-query)
+  :config
+  ;; default to the environment variable
+  (unless (getenv "OPENAI_API_KEY")
+    ;; otherwise read the api key in from the config file
+    (setq openai-config-file "~/.openai")
+    (message "OPENAI_API_KEY not defined. Attempting to read from file: %s"
+             openai-config-file)
+    (if (file-exists-p openai-config-file)
+        (let ((config-file-sk
+               (with-temp-buffer
+                 (insert-file-contents file-path)
+                 (buffer-string))))
+          (setenv "OPENAI_API_KEY" config-file-sk))
+      (message "openai config file does not exist. Exiting..."))))
+
+
 (use-package lsp-mode
   :ensure t
   :init
   (setq lsp-keymap-prefix "C-c l")
-  :commands lsp)
+  :commands lsp
+  :hook (js-mode . (lambda ()
+                     (electric-indent-mode -1)
+                     (lsp))))
+
 
 (use-package lsp-pyright
   :ensure t
@@ -118,17 +154,82 @@
   :ensure t
   :mode ("\\.rkt\\'" . racket-mode)
   :config
-  (setq racket-command-port 9091))
+  (setq racket-command-port 9091)
+  (racket-unicode-input-method-enable))
+
+(use-package react-snippets
+  :ensure t)
 
 (use-package swift-mode
   :ensure t
   :hook (swift-mode . (lambda () (lsp))))
+
+(use-package solidity-mode
+  :ensure t)
+
+(use-package tree-sitter
+  :ensure t)
+
+(use-package tree-sitter-langs
+  :ensure t)
+
+(defun setup-tide-mode ()
+  "hook to setup tide"
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+;; (use-package typescript-mode
+;;   :ensure t)
+
+;; (use-package tide
+;;   :ensure t
+;;   :after (typescript-mode company flycheck)
+;;   :init
+;;   (progn
+;;     (setq company-tooltip-align-annotations t)
+;;     (setq flycheck-check-syntax-automatically '(save mode-enabled)))
+;;   :hook ((typescript-mode . tide-setup)
+;;          (typescript-mode . tide-hl-identifier-mode)
+;;          (before-save . tide-format-before-save)))
+
+;; (use-package web-mode
+;;   :ensure t
+;;   :mode ("\\.tsx\\'" . web-mode)
+;;   :config
+;;   (flycheck-add-mode 'typescript-tslint 'web-mode)
+;;   :hook (web-mode . (lambda ()
+;;                       (when (string-equal
+;;                              "tsx"
+;;                              (file-name-extension buffer-file-name))
+;;                         (setup-tide-mode))))
+;;   :custom
+;;   (web-mode-markup-indent-offset 2)
+;;   (web-mode-css-indent-offset 2)
+;;   (web-mode-code-indent-offset 2))
+
+(use-package web-mode
+  :ensure t
+  :mode ("\\.tsx\\'" . web-mode)
+  :config
+  (flycheck-add-mode 'typescript-tslint 'web-mode)
+  :custom
+  (web-mode-markup-indent-offset 2)
+  (web-mode-css-indent-offset 2)
+  (web-mode-code-indent-offset 2))
 
 (use-package whitespace
   :ensure t
   :init
   (add-hook 'before-save-hook
             'whitespace-cleanup))
+
+(use-package yasnippet
+  :ensure t
+  :hook (js-mode . yas-minor-mode-on))
 
 (use-package zenburn-theme
   :ensure t
@@ -150,13 +251,22 @@
                 (lambda ()
                   (interactive)
                   (other-window -1)))
+(global-set-key (kbd "C-x p")
+                (lambda ()
+                  (interactive)
+                  (other-window -1)))
+(add-hook
+ 'after-change-major-mode-hook
+ #'(lambda ()
+     (electric-indent-mode -1)))
+(setq js-indent-level 2)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zenburn-theme swift-mode racket-mode proof-general paredit multiple-cursors magit lsp-sourcekit lsp-pyright lsp-mode ess exec-path-from-shell company-coq dash use-package)))
+   '(cmake-mode ein bind-key company company-math compat f git-commit ht lv magit-section markdown-mode math-symbol-lists s spinner transient with-editor yasnippet zenburn-theme swift-mode racket-mode proof-general paredit multiple-cursors magit lsp-sourcekit lsp-pyright lsp-mode ess exec-path-from-shell company-coq dash use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
