531b3331ca0644ec30e1becc899123938bc8a050
bkc39 <pl.proofs@gmail.com>
2024-06-10 15:52:17 -0400

    remove old shellgpt.el and flycheck

--- init.el
+++ init.el
@@ -89,19 +89,6 @@
   :config
   (exec-path-from-shell-initialize))
 
-(use-package flycheck)
-
-(use-package shellgpt
-  :straight (:host github
-             :repo "bkc39/shellgpt.el"
-             :files ("dist" "*.el"))
-  :bind ("C-c q" . shellgpt:quick-ask)
-  :custom
-  (shellgpt:repl-chat-name
-   (concat "emacs-"
-           (format-time-string "%Y%m%d"
-                               (current-time)))))
-
 (use-package go-mode
   :after lsp-mode
   :hook (go-mode . (lambda ()
