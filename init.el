e109a3a5c38d8f3a89e174dcadf03225146372e4
Ben Carriel <pl.proofs@gmail.com>
2025-12-19 18:03:19 -0500

    formatting

--- init.el
+++ init.el
@@ -10,8 +10,10 @@
 
 ;;; Code:
 
-(add-to-list 'load-path (expand-file-name "elisp" user-emacs-directory))
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(add-to-list
+ 'load-path (expand-file-name "elisp" user-emacs-directory))
+(setq
+ custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file nil 'nomessage))
 
